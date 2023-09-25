92bfd942d7f9007e88e4888cab058b4fc2dd5fdd
nchore <nchoreanthony@gmail.com>
2023-09-26 01:38:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function countDown(startingNumber) {
+    if (startingNumber < 0) {
+      console.log("Please provide a positive integer.");
+      return;
+    }
+  
+    while (startingNumber >= 0) {
+      console.log(startingNumber);
+      startingNumber--;
+    }
+  }
+  
+  countDown(10);
